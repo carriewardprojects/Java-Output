ec0bb554124263c78fe73e89a9ca7a093c389c89
Carrie Ward <carriewardprojects@gmail.com>
2017-04-04 23:09:00 -0700

    java source code

--- displayOutput.java
+++ displayOutput.java
@@ -3,8 +3,12 @@ package displayOutput;
 public class displayOutput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello there");
+		
+		// a simple java program that displays various outputs
+		System.out.println("    J     A     V     V     A");
+		System.out.println("    J    A A     V   V     A A");
+		System.out.println("J   J   AAAAA     V V     AAAAA");
+		System.out.println(" J J   A     A     V     A     A");
 	}
 
 }
