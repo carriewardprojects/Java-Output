220e724f413d5f1f56a56a35f83ce06848a8b351
Carrie Ward <carriewardprojects@gmail.com>
2017-04-04 22:13:01 -0700

    source code

---
+++ displayOutput.java
@@ -0,0 +1,10 @@
+package displayOutput;
+
+public class displayOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello there");
+	}
+
+}
